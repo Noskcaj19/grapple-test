262ae77b4378f6d2f40367c4c499d1a8f222f32f
Joshua Steege <joshuasteege@gmail.com>
2023-05-02 18:54:47 -0400

    Pointer no longer collides with hidden UI

--- scenes/LeftActionHandler.gd
+++ scenes/LeftActionHandler.gd
@@ -3,6 +3,8 @@ extends Node
 @onready var _controller := XRHelpers.get_xr_controller(self)
 @export var ui: Node3D
 
+var ui_layers : int
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_controller.button_pressed.connect(_handle_button)
@@ -15,6 +17,14 @@ func _handle_button(button):
 
 func _toggle_ui():
 	if ui.visible:
+		ui.collision_layer = disable_bit(ui.collision_layer, 20)
 		ui.hide()
 	else:
+		ui.collision_layer = enable_bit(ui.collision_layer, 20)
 		ui.show()
+
+func enable_bit(mask: int, index: int) -> int:
+	return mask | (1 << index)
+
+func disable_bit(mask: int, index: int) -> int:
+	return mask & ~(1 << index)
