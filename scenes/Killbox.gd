ba4d4088a51827741da50a1a69936286686bb567
Joshua Steege <joshuasteege@gmail.com>
2023-05-01 21:57:08 -0400

    Fixed killbox

--- scenes/Killbox.gd
+++ scenes/Killbox.gd
@@ -1,17 +1,4 @@
 extends Area3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_body_exited(body):
-	print("body")
-	print(body.to_string())
-	# Reload scene
+func _on_body_exited(_body):
 	get_tree().reload_current_scene()
