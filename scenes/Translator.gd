cdd0d61a5499d1c447d73b48f441392f971ed3cb
Noskcaj19 <Noskcaj19@users.noreply.github.com>
2023-05-03 09:08:29 -0400

    Rearrange tree, make floor blue

---
+++ scenes/Translator.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var x = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x = sin(x)
+	x += .01
