410ed748672993d83f8c3ef2970bfa45ba32f935
Noskcaj19 <Noskcaj19@users.noreply.github.com>
2023-05-03 10:44:07 -0400

    Add slowdown button (right b)

--- scenes/VelocityLimiter.gd
+++ scenes/VelocityLimiter.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var character: CharacterBody3D = get_parent()
 @export var limit = 50
+@export var slowdown_limit = 20
+@onready var right_hand = XRHelpers.get_right_controller(self)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +12,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if right_hand.get_input("by_button"):
+		if character.velocity.length() > slowdown_limit:
+			character.velocity = _clamp_norm(character.velocity, slowdown_limit)
+
 	if character.velocity.length() > limit:
 		character.velocity = _clamp_norm(character.velocity, limit)
-		print(character.velocity)
+
 
 func _clamp_norm(v: Vector3, n_max: float):
 	var n = sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2)
